caf912536bf5da1147265f95e6c2cebba8da0797
TaslimuddinAnsari <taslimuddin.ansari@madgicaltechdom.com>
2022-10-01 17:43:09 +0530

    basic of action, reducer, store

--- src/React-Redux/index.js
+++ src/React-Redux/index.js
@@ -1,5 +1,7 @@
+const redux = require('redux');
+const createStore = redux.createStore
 
-// Action is function thet return object
+// Action is function and return object
 const BUY_CAKE = 'BUY_CAKE'
 function buyCake(){
     return {
@@ -7,3 +9,33 @@ function buyCake(){
         info:'First redux action'
      }
 }
+
+// (prevoiusState, action) => newState
+
+const initialState = {
+    numberofCake :10
+}
+
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case BUY_CAKE : return{
+            ...state,
+            numberofCake: state.numberofCake -1 
+        }
+
+        default: return state
+    }
+}
+
+
+// it accept reducer function as parameter
+const store = createStore(reducer)
+
+console.log('initial state', store.getStore())
+
+const unSubscriber = store.subscribe(() => console.log('Update state', store.getStore()))
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+
+unSubscriber()
